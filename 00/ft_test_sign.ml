0766199607bc91f04432a0ab6ab726335bd3ce66
ngoc2012 <ngoc2012@yahoo.com>
2025-01-24 20:20:02 +0100

    tempory

--- 00/ft_test_sign.ml
+++ 00/ft_test_sign.ml
@@ -1,2 +1,3 @@
-ft_test_sign (n int) -> string:
-        if n < 0 "negative" else "positive"
+let ft_test_sign (n: int) : string:
+        if n < 0 print_endline "negative" else print_endline "positive"
+let 
