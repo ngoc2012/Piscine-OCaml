10611661a67549f5dcecced941c0f4b2f790bc6a
ngoc2012 <ngoc2012@yahoo.com>
2025-01-25 18:10:01 +0100

    tempory

--- 01/ex04/fibonacci.ml
+++ 01/ex04/fibonacci.ml
@@ -1,11 +1,23 @@
-let rec fibonacci n =
+let fibonacci n =
   if n = 0 then 0
   else if n < 0 then (-1)
-  else if n = 1 then 1
-  else fibonacci (n - 1) + fibonacci (n - 2)
+  else if n = 1 || n = 2 then 1
+  else (
+    let rec fibo x y s e =
+      if s = e then y
+      else fibo y (x + y) (s + 1) n 
+    in
+    fibo 1 2 3 n
+  )
 
 let () =
   print_int (fibonacci (-42));
+  print_char '\n';
   print_int (fibonacci 1);
+  print_char '\n';
   print_int (fibonacci 3);
+  print_char '\n';
   print_int (fibonacci 6);
+  print_char '\n';
+  print_int (fibonacci 15);
+  print_char '\n';
