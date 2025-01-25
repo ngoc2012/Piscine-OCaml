970e1d781b2b8f09bfe751d86be09c9e07f8b2c4
ngoc2012 <ngoc2012@yahoo.com>
2025-01-25 22:10:01 +0100

    tempory

--- 01/ex09/leibniz_pi.ml
+++ 01/ex09/leibniz_pi.ml
@@ -1,9 +1,9 @@
-let rec ft_sum f s e =
+let leibniz_pi =
+  let pi0 = 4. *. atan 1.
+  and tolerance = 0.00001 in
   if s > e then nan
   else if s = e then f s
   else f s +. ft_sum f (s + 1) e
 
 let () =
-  print_endline (string_of_float (ft_sum (fun x -> float_of_int x) 1 5));
-  print_endline (string_of_float (ft_sum (fun i -> float_of_int (i * i)) 1 10));
-  print_endline (string_of_float (ft_sum (fun i -> float_of_int (i * i)) 100 10));
+  print_endline (string_of_float (leibniz_pi));
