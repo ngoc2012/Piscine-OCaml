ee3cb36f7f465915f959284dd95bf133189266dc
ngoc2012 <ngoc2012@yahoo.com>
2025-01-25 22:20:01 +0100

    tempory

--- 01/ex09/leibniz_pi.ml
+++ 01/ex09/leibniz_pi.ml
@@ -1,9 +1,11 @@
 let leibniz_pi =
   let pi0 = 4. *. atan 1.
-  and tolerance = 0.00001 in
-  if s > e then nan
-  else if s = e then f s
-  else f s +. ft_sum f (s + 1) e
+  and tolerance = 0.00001
+  let rec pi =
+    if pi -. pi0 < tolerance then pi
+    else 4. * 
+  in
+  pi
 
 let () =
   print_endline (string_of_float (leibniz_pi));
