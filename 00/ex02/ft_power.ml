2bbf1a56ed3cddbd1530382d396c95a2631e3a56
ngoc2012 <ngoc2012@yahoo.com>
2025-01-25 07:20:01 +0100

    tempory

--- 00/ex02/ft_power.ml
+++ 00/ex02/ft_power.ml
@@ -1,3 +1,12 @@
-let rec ft_power (n: int, p: int) : int =
-        if p = 0 then 1
-        else n * (ft_power (n, p - 1))
+let rec ft_power (n: int) (p: int) : int =
+  if p = 0 then 1
+  else if n = 0 then 0
+  else n * (ft_power n (p - 1))
+
+let () =
+  print_int (ft_power 2 4);
+  print_char ('\n');
+  print_int (ft_power 3 0);
+  print_char ('\n');
+  print_int (ft_power 0 5);
+  print_char ('\n');
---
+++ 00/ex03/ft_print_alphabet.ml
@@ -0,0 +1,5 @@
+let rec ft_print_alphabet () =
+  
+        for i = 97 to 122 do
+                print_char (char_of_int i)
+        done
