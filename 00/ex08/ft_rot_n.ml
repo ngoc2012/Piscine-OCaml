9b65c46526e5077fa1bdcaaf7b1ff5281e80ff7d
ngoc2012 <ngoc2012@yahoo.com>
2025-01-25 09:00:01 +0100

    tempory

--- 00/ex08/ft_rot_n.ml
+++ 00/ex08/ft_rot_n.ml
@@ -1,4 +1,29 @@
 let ft_rot_n (n: int) (s: string): string =
   let rot (c: char): char =
-    char_of_int (((int_of_char c) - 97 + n) mod 26 + 97)
-  String.map rot s
+    let nc = int_of_char c in
+    let na = int_of_char 'a' in
+    let nz = int_of_char 'z' in
+    let nA = int_of_char 'A' in
+    let nZ = int_of_char 'Z' in
+    if (nc >= na) && (nc <= nz) then
+      char_of_int ((nc - na + n) mod nz)
+    else if (nc >= nA) && (nc <= nZ) then
+      char_of_int ((nc - nA + n) mod nZ)
+    else c
+  in String.map rot s
+
+let () =
+  let res = ft_rot_n 1 "abcdefghijklmnopqrstuvwxyz" in
+  print_endline res;
+  let res = ft_rot_n 13 "abcdefghijklmnopqrstuvwxyz" in
+  print_endline res;
+  let res = ft_rot_n 42 "0123456789" in
+  print_endline res;
+  let res = ft_rot_n 2 "OI2EAS67B9" in
+  print_endline res;
+  let res = ft_rot_n 0 "Damned !" in
+  print_endline res;
+  let res = ft_rot_n 42 "" in
+  print_endline res;
+  let res = ft_rot_n 1 "NBzlk qnbjr !" in
+  print_endline res
