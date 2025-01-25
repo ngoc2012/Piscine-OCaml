7970810afc77572d43facb24d88e325a7533382b
ngoc2012 <ngoc2012@yahoo.com>
2025-01-25 08:10:01 +0100

    tempory

--- 00/ex04/ft_print_comb.ml
+++ 00/ex04/ft_print_comb.ml
@@ -4,11 +4,18 @@ let ft_print_comb () =
         print_int i;
         print_int j;
         print_int k;
-        print_string ", "
+        if (i != 7) then print_string ", " else print_string "\n"
     );
-    if (k < 9) then comb i j (k + 1);
-    if (j < 9) then comb i (j + 1) (j + 2);
-    if (i < 9) then comb (i + 1) (i + 2) (i + 3)
+    if (k < 9) then comb i j (k + 1)
+    else (
+      if (j < 8) then (
+        comb i (j + 1) (j + 2)
+      ) else (
+        if (i < 7) then (
+          comb (i + 1) (i + 2) (i + 3)
+        )
+      )
+    )
   ) in
   comb 0 1 2
 
