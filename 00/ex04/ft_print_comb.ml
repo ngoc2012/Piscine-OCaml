5458d6e3535269f9ddda4d0100df99c68001cb79
ngoc2012 <ngoc2012@yahoo.com>
2025-01-25 07:50:01 +0100

    tempory

--- 00/ex04/ft_print_comb.ml
+++ 00/ex04/ft_print_comb.ml
@@ -2,12 +2,28 @@ let ft_print_comb () =
   let i = 0 in
   let j = 1 in
   let k = 2 in
-  let print (i: int) (j: int) (k: int) = (
-    print_int i;
-    print_int j;
-    print_int k
-  )
-  in
+  let rec comb (i: int) (j: int) (k: int) = (
+    if (i < 9 && j < 9 && k < 9) then (
+      if (i != j && j != k && i != k) then (
+        print_int i;
+        print_int j;
+        print_int k
+        print_string ", "
+      ) else (
+        if (k < 9) then (
+          comb i j (k + 1)
+        ) else if (j < 9) then (
+          comb i (j + 1) 2
+        ) else if (i < 9) then (
+          comb (i + 1) 1 2
+        )
+        ) else (
+          comb i (j + 1) 2
+        )
+        if 
+      )
+    )
+  ) in
   print i j k
 
 let () = ft_print_comb ()
