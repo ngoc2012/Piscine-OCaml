b5a87dc608c437e03cd475bf8526c0c1e9f80dab
ngoc2012 <ngoc2012@yahoo.com>
2025-01-25 05:50:01 +0100

    tempory

--- 00/ex00/ft_test_sign.ml
+++ 00/ex00/ft_test_sign.ml
@@ -1,2 +1,9 @@
 let ft_test_sign (n: int) =
   if n < 0 then print_endline "negative" else print_endline "positive"
+
+let () =
+  let test_values = [42; 0; -42] in
+  List.iter (fun n ->
+    Printf.printf "Test with [%d]: " n;
+    ft_test_sign n
+  ) test_values
