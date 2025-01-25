1c6cee433562a0d77b9f8305a765c757b7804458
ngoc2012 <ngoc2012@yahoo.com>
2025-01-25 15:50:01 +0100

    tempory

--- 01/ex01/repeat_string.ml
+++ 01/ex01/repeat_string.ml
@@ -1,5 +1,15 @@
-let repeat_string ?str:(s = "x") n =
-  if n < 0 then "Error"
-  else if n = 0 then ""
-  else "x" ^ repeat_x (n - 1)
+let rec repeat_string ?str:(s = "x") n =
+  let repeat s n =
+    if n < 0 then "Error"
+    else if n = 0 then ""
+    else s ^ repeat_string ~str:s (n - 1)
+  in
+  repeat s n
 
+let () =
+  print_endline(repeat_string (-1));
+  print_endline(repeat_string 0);
+  print_endline(repeat_string ~str:"Toto" 1);
+  print_endline(repeat_string 2);
+  print_endline(repeat_string ~str:"a" 5);
+  print_endline(repeat_string ~str:"what" 3);
