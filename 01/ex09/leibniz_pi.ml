4d466585fdd75bb901719266ec6b1315fdc9f6dc
ngoc2012 <ngoc2012@yahoo.com>
2025-01-26 03:30:02 +0100

    tempory

--- 01/ex09/leibniz_pi.ml
+++ 01/ex09/leibniz_pi.ml
@@ -1,24 +1,27 @@
 let leibniz_pi delta =
-  let pi0 = 4. *. atan 1. in
-  let ft_power n p =
-    if n = 0 then 0
-    else if p = 0 then 1
-    else (
-      let rec pow n p np =
-        if p = 0 then np
-        else pow n (p - 1) (n * np)
-      in pow n p 1
-    )
-  in
-  let rec sum f s e t =
-    let res = t +. f s in
-    if s > e then nan
-    (* else if (s = e) || ((res -. pi0) <= delta) then res *)
-    else if s = e then (t +. f s)
-    else sum f (s + 1) e res
-  in
-  let fi i = 4. *. (float_of_int (ft_power (-1) i)) /. (2. *. float_of_int(i) +. 1.) in
-  sum fi 1 10000 4. 
+  if delta <= 0. then -1.
+  else (
+    let pi0 = 4. *. atan 1. in
+    let ft_power n p =
+      if n = 0 then 0
+      else if p = 0 then 1
+      else (
+        let rec pow n p np =
+          if p = 0 then np
+          else pow n (p - 1) (n * np)
+        in pow n p 1
+      )
+    in
+    let rec sum f s e t =
+      let res = t +. f s in
+      if s > e then nan
+      else if (s = e) || (abs_float (res -. pi0) <= delta) then res
+      (* else if s = e then res *)
+      else sum f (s + 1) e res
+    in
+    let fi i = 4. *. (float_of_int (ft_power (-1) i)) /. (2. *. float_of_int(i) +. 1.) in
+    sum fi 1 10000 4. 
+  )
 
 let () =
   print_endline (string_of_float (leibniz_pi 0.001));
