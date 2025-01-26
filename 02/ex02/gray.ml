2e476b1c2dba81f2dc3cdb81fca163360178561a
ngoc2012 <ngoc2012@yahoo.com>
2025-01-27 00:50:01 +0100

    tempory

--- 02/ex02/gray.ml
+++ 02/ex02/gray.ml
@@ -1,8 +1,21 @@
 let gray n =
-  if n <= 0 then []
-  let rec aux n acc =
-    if n = 1 then acc
-    else aux (n - 1) (n :: acc)
-  in aux n ["0"; "1"]
+  if n <= 0 then print_endline "n must be greater than 0"
+  else
+    let rec rev li lo = match li with
+      | [] -> lo
+      | h :: t -> rev t (h :: lo)
+    in
+    let rec aux n acc =
+      if n = 1 then acc
+      else aux (n - 1) (n :: acc)
+    in
+    let print strings = 
+      begin
+        print_string "[";
+        print_string (String.concat ", " strings);
+        print_endline "]";
+      end
+    in
+    print aux n ["0"; "1"]
 let () =
 
