de2531dc85ba594d2d14c05e8dc395e554fd1043
ngoc2012 <ngoc2012@yahoo.com>
2025-01-26 16:40:01 +0100

    tempory

--- 02/ex00/encode.ml
+++ 02/ex00/encode.ml
@@ -1,7 +1,11 @@
 let encode l =
   let rec aux l acc = match l with
-      | [] -> acc
-      | h :: t ->
-                    let count = List.length (List.filter ((=) h) t) in
-                    aux t ((h, count) :: acc)
+    | [] -> acc
+    | h :: t ->
+      let count = List.length (List.filter ((=) h) t) in
+      aux t ((h, count) :: acc)
   in aux l []
+
+let () =
+  let l = ['a'; 'a'; 'a'; 'a'; 'b'; 'c'; 'c'; 'a'; 'a'; 'd'; 'e'; 'e'; 'e'; 'e'] in
+  print_endline (string_of_list (encode l))
