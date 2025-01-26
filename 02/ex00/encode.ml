80d633edcae1ddb507548dcd38d5fdf74410b7db
ngoc2012 <ngoc2012@yahoo.com>
2025-01-26 18:10:01 +0100

    tempory

--- 02/ex00/encode.ml
+++ 02/ex00/encode.ml
@@ -1,10 +1,10 @@
 let encode l =
-  let rec aux l acc = match l with
+  let rec aux l acc count = match l with
     | [] -> acc
     | h :: t ->
       let count = List.length (List.filter ((=) h) t) in
       aux t ((h, count) :: acc)
-  in aux l []
+  in aux l [] 0
 
 let () =
   let string_of_list lst =
