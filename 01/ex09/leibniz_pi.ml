54b05eb5aaf6bef93fd1598f1f8d20a83548832d
ngoc2012 <ngoc2012@yahoo.com>
2025-01-26 03:10:01 +0100

    tempory

--- 01/ex09/leibniz_pi.ml
+++ 01/ex09/leibniz_pi.ml
@@ -1,6 +1,6 @@
 let leibniz_pi =
-  (* let pi0 = 4. *. atan 1. in *)
-  (* let tolerance = 0.00001 in *)
+  let pi0 = 4. *. atan 1. in
+  let tolerance = 0.00001 in
   (*  if t -. pi0 <= tolerance then (t +. f s) *)
   let ft_power n p =
     if n = 0 then 0
@@ -14,11 +14,17 @@ let leibniz_pi =
   in
   let rec sum f s e t =
     if s > e then nan
-    else if s = e then (t +. f s)
+    else if (s = e) || ((t -. pi0) <= tolerance) then (t +. f s)
     else sum f (s + 1) e (t +. f s)
   in
-  let fi i = float_of_int (ft_power (-1) i) in
-  sum fi 1 4 0. 
+  let fi i = 4. *. (float_of_int (ft_power (-1) i)) /. (2. *. float_of_int(i) +. 1.) in
+  sum fi 1 1000 4. 
 
 let () =
   print_endline (string_of_float (leibniz_pi));
+  let pi0 = 4. *. atan 1. in
+  let tolerance = 0.00001 in
+  let t = 3.14259165434 in
+  print_endline (string_of_float (pi0));
+  print_endline (string_of_float (t -. pi0));
+  print_endline (string_of_bool ((t -. pi0) <= tolerance));
