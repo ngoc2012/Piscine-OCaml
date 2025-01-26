4d5abda4c3c7d1b89d825a835ccf1262632cf7a2
ngoc2012 <ngoc2012@yahoo.com>
2025-01-26 02:10:01 +0100

    tempory

--- 01/ex09/leibniz_pi.ml
+++ 01/ex09/leibniz_pi.ml
@@ -1,15 +1,16 @@
 let leibniz_pi =
-  let pi0 = 4. *. atan 1.
-  and tolerance = 0.00001
+  let pi0 = 4. *. atan 1. in
+  let tolerance = 0.00001 in
+  let rec sum f t =
+    if t -. pi0 <= tolerance then (t +. f s)
+    else sum f (s + 1) e (t +. f s)
+  in
   let rec sum f s e t =
     if s > e then nan
     else if s = e then (t +. f s)
     else sum f (s + 1) e (t +. f s)
   in
-  let rec pi i =
-    if pi -. pi0 < tolerance then pi
-    else 4. * 
-  in
+  (* in sum f s e 0. *)
   pi 0
 
 let () =
