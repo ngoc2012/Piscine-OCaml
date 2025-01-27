090871b4890cb33c735ce823ac70f8a580206b2c
ngoc2012 <ngoc2012@yahoo.com>
2025-01-27 01:30:02 +0100

    tempory

--- 02/ex02/gray.ml
+++ 02/ex02/gray.ml
@@ -1,6 +1,6 @@
-let gray n =
-  if n <= 0 then print_endline "n must be greater than 0"
-  else
+let gray n = match n with
+  | 0 -> print_endline "n must be greater than 0"
+  | _ ->
     let rec rev li lo = match li with
       | [] -> lo
       | h :: t -> rev t (h :: lo)
@@ -11,7 +11,10 @@ let gray n =
     in
     let rec aux n output = match n with
       | 1 -> output
-      | _ -> aux (n - 1) (insert "1" output []);
+      | _ ->
+        let r = insert "1" output [] in
+        let l = insert "0" output r in
+        aux (n - 1) l
     in
     let print strings = 
       begin
@@ -20,7 +23,7 @@ let gray n =
         print_endline "]";
       end
     in
-    print (aux n ["0"; "1"]);
+    print (aux n ["0"; "1"])
 
 let () =
   gray 2
