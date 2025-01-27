f08fa454d65f1af75099564a0ba197f267b41fb1
minh-ngu <minh-ngu@ngoc.myguest.virtualbox.org>
2025-01-27 17:10:01 +0100

    tempory

--- 02/ex07/ribosome.ml
+++ 02/ex07/ribosome.ml
@@ -17,7 +17,7 @@ type rna = nucleobase list
 type protein = aminoacid list
 
 let generate_nucleotide c =
-  let char_to_n c = match c with
+  let char_to_n = function
     | 'A' -> A
     | 'C' -> C
     | 'T' -> T
@@ -46,13 +46,13 @@ let generate_helix n : helix =
 
 let helix_to_string (hx: helix) =
   let rec aux hx acc =
-    let n_to_string n = match n with
+    let n_to_string = function
       | A -> "A"
       | C -> "C"
       | T -> "T"
       | U -> "U"
       | G -> "G"
-      | None -> " "
+      | _ -> " "
     in
     match hx with
       | [] -> acc
@@ -68,7 +68,7 @@ let rec rev li lo = match li with
 
 let complementary_helix (hx: helix) : helix =
   let rec aux hx acc =
-    let n_to_c n = match n with
+    let n_to_c = function
       | A -> 'T'
       | T -> 'A'
       | C -> 'G'
@@ -85,7 +85,7 @@ let complementary_helix (hx: helix) : helix =
 
 let generate_rna (hx: helix) : rna =
   let rec aux hx acc =
-    let pairing n = match n with
+    let pairing = function
       | A -> U
       | T -> A
       | C -> G
@@ -102,13 +102,13 @@ let generate_rna (hx: helix) : rna =
 
 let rna_to_string (r: rna) =
   let rec aux r acc =
-    let n_to_string n = match n with
+    let n_to_string = function
       | A -> "A"
       | C -> "C"
       | T -> "T"
       | U -> "U"
       | G -> "G"
-      | None -> " "
+      | _ -> " "
     in
     match r with
       | [] -> acc
@@ -124,6 +124,62 @@ let generate_bases_triplets (r: rna) : (nucleobase * nucleobase * nucleobase) li
     | _ -> acc
   in
   rev (aux r []) []
+
+let string_of_protein = function
+    | Ala -> "Ala"
+    | Arg -> "Arg"
+    | Asn -> "Asn"
+    | Asp -> "Asp"
+    | Cys -> "Cys"
+    | Gln -> "Gln"
+    | Glu -> "Glu"
+    | Gly -> "Gly"
+    | His -> "His"
+    | Ile -> "Ile"
+    | Leu -> "Leu"
+    | Lys -> "Lys"
+    | Met -> "Met"
+    | Phe -> "Phe"
+    | Pro -> "Pro"
+    | Ser -> "Ser"
+    | Thr -> "Thr"
+    | Trp -> "Trp"
+    | Tyr -> "Tyr"
+    | Val -> "Val"
+    | Stop -> "Stop"
+    | _ -> "None"
+
+let decode_arn (rna_seq: rna) : protein =
+  let triplet_to_aminoacid = function
+    | (U, A, A) | (U, A, G) | (U, G, A) -> Stop
+    | (G, C, A) | (G, C, C) | (G, C, G) | (G, C, U) -> Ala
+    | (A, G, A) | (A, G, G) | (C, G, A) | (C, G, C) | (C, G, G) | (C, G, U) -> Arg
+    | (A, A, C) | (A, A, U) -> Asn
+    | (G, A, C) | (G, A, U) -> Asp
+    | (U, G, C) | (U, G, U) -> Cys
+    | (C, A, A) | (C, A, G) -> Gln
+    | (G, A, A) | (G, A, G) -> Glu
+    | (G, G, A) | (G, G, C) | (G, G, G) | (G, G, U) -> Gly
+    | (C, A, C) | (C, A, U) -> His
+    | (A, U, A) | (A, U, C) | (A, U, U) -> Ile
+    | (C, U, A) | (C, U, C) | (C, U, G) | (C, U, U) | (U, U, A) | (U, U, G) -> Leu
+    | (A, A, A) | (A, A, G) -> Lys
+    | (A, U, G) -> Met
+    | (U, U, C) | (U, U, U) -> Phe
+    | (C, C, C) | (C, C, A) | (C, C, G) | (C, C, U) -> Pro
+    | (U, C, A) | (U, C, C) | (U, C, G) | (U, C, U) | (A, G, U) | (A, G, C) -> Ser
+    | (A, C, A) | (A, C, C) | (A, C, G) | (A, C, U) -> Thr
+    | (U, G, G) -> Trp
+    | (U, A, C) | (U, A, U) -> Tyr
+    | (G, U, A) | (G, U, C) | (G, U, G) | (G, U, U) -> Val
+    | _ -> Stop  (* Default case for safety *)
+  in
+  let rec aux acc = function
+    | [] -> acc
+    | (U, A, A) :: _ | (U, A, G) :: _ | (U, G, A) :: _ -> List.rev (Stop :: acc)  (* Stop translation *)
+    | triplet :: rest -> aux ((triplet_to_aminoacid triplet) :: acc) rest
+  in
+  aux [] (generate_bases_triplets rna_seq)
 
 let () =
   let hx = generate_helix 100 in
