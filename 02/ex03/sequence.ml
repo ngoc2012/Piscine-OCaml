ec88e566640202bfaf71b49d52a9c125be0c0f87
minh-ngu <minh-ngu@ngoc.myguest.virtualbox.org>
2025-01-27 12:30:01 +0100

    tempory

--- 02/ex03/sequence.ml
+++ 02/ex03/sequence.ml
@@ -1,31 +1,28 @@
 let sequence n = match n with
   | n when n <= 0 -> []
   | _ ->
-    let print_tuple (c, n) =
-      print_int c;
-      print_int n
-    in
-    let print list_strings =
+    let print list_int =
       | [] -> ()
-      | [t] -> print_tuple t
+      | [t] -> print_int t
       | h :: t ->
-        print_tuple h;
+        print_int h;
         print t
     in
     let rec aux l acc count = match l with
       | [] -> acc
-      | [t] -> (t, count + 1) :: acc
+      | [t] -> (count + 1) :: t :: acc
       | h :: s :: t ->
         if h = s then
           aux (s :: t) acc (count + 1)
         else
-          aux (s :: t) ((h, count + 1) :: acc) 0
+          aux (s :: t) ((count + 1) :: h :: acc) 0
     in
     let rec loop n last = match n with
-      | 1 -> "1"
+      | 1 -> [1]
       | _ ->
-        loop (n - 1) (string_of_int n ^ last)
+        loop (n - 1) (aux last [] 0)
     in
-    print (loop n "")
+    print (loop n [])
 
 let () =
+  sequence 1;;
