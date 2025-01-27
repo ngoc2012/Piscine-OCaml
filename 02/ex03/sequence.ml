05fd60941ee53f83e9162be2721c3d8068dd972e
minh-ngu <minh-ngu@ngoc.myguest.virtualbox.org>
2025-01-27 12:10:01 +0100

    tempory

--- 02/ex03/sequence.ml
+++ 02/ex03/sequence.ml
@@ -1,3 +1,20 @@
-let sequence n =
+let sequence n = match n with
+  | n when n <= 0 -> []
+  | _ ->
+    let rec aux l acc count = match l with
+      | [] -> acc
+      | [t] -> (t, count + 1) :: acc
+      | h :: s :: t ->
+        if h = s then
+          aux (s :: t) acc (count + 1)
+        else
+          aux (s :: t) ((h, count + 1) :: acc) 0
+    in
+    let rec loop n last = match n with
+      | 1 -> "1"
+      | _ ->
+        if n = 1 then acc
+        loop (n - 1) (string_of_int n ^ last)
+    in loop n ""
 
 let () =
