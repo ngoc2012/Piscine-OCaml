d532c792a162ba3b7552a0fc5217a4bb491a1f85
ngoc2012 <ngoc2012@yahoo.com>
2025-01-27 01:40:02 +0100

    tempory

--- 02/ex02/gray.ml
+++ 02/ex02/gray.ml
@@ -1,29 +1,27 @@
 let gray n = match n with
-  | 0 -> print_endline "n must be greater than 0"
+  | n when n < 1 -> print_endline "n must be greater than 0"
   | _ ->
     let rec rev li lo = match li with
       | [] -> lo
       | h :: t -> rev t (h :: lo)
     in
-    let rec insert s li lo = match li with
+    let rec prefix s li lo = match li with
       | [] -> lo
-      | h :: t -> insert s t ((s ^ h) :: lo)
+      | h :: t -> prefix s t ((s ^ h) :: lo)
     in
     let rec aux n output = match n with
       | 1 -> output
       | _ ->
-        let r = insert "1" output [] in
-        let l = insert "0" output r in
+        let r = prefix "1" output [] in
+        let l = prefix "0" (rev output []) r in
         aux (n - 1) l
     in
-    let print strings = 
-      begin
-        print_string "[";
-        print_string (String.concat ", " strings);
-        print_endline "]";
-      end
-    in
-    print (aux n ["0"; "1"])
+    print_endline (String.concat " " (aux n ["0"; "1"]))
 
 let () =
-  gray 2
+  gray (-1);
+  gray 0;
+  gray 1;
+  gray 2;
+  gray 3;
+  gray 4
