99805c92b49e8af5119c4d603be691a783b29d28
minh-ngu <minh-ngu@ngoc.myguest.virtualbox.org>
2025-01-28 16:30:01 +0100

    tempory

--- 10/ex02/Card.ml
+++ 10/ex02/Card.ml
@@ -83,3 +83,95 @@ module Value = struct
         else check (s :: t)
     in check all
 end
+
+module Card = struct
+  type t = { value: Value.t; color: Color.t }
+
+  let newCard value color = { value; color }
+
+  let allSpades =
+    List.map (fun v -> newCard v Color.Spade) Value.all
+
+  let allHearts =
+    List.map (fun v -> newCard v Color.Heart) Value.all
+
+  let allDiamonds =
+    List.map (fun v -> newCard v Color.Diamond) Value.all
+
+  let allClubs =
+    List.map (fun v -> newCard v Color.Club) Value.all
+
+  let all = allSpades @ allHearts @ allDiamonds @ allClubs
+
+  let getValue card = card.value
+
+  let getColor card = card.color
+
+  let toString card =
+    let value_str = Value.toString card.value in
+    let color_str = Color.toString card.color in
+    value_str ^ color_str
+
+  let toStringVerbose card =
+    let value_str = Value.toStringVerbose card.value in
+    let color_str = Color.toStringVerbose card.color in
+    "Card(" ^ value_str ^ ", " ^ color_str ^ ")"
+
+  let compare card1 card2 =
+    if card1.value = card.value && card1.color = card.color then true
+    else false
+
+  let max card1 card2 =
+    if compare card1 card2 >= 0 then card1 else card2
+
+  let min card1 card2 =
+    if compare card1 card2 <= 0 then card1 else card2
+
+  let best cards =
+    match cards with
+    | [] -> invalid_arg "Card.best"
+    | hd :: tl -> List.fold_left (fun best card -> if compare card best <= 0 then card else best) hd tl
+
+  let isOf card color = card.color = color
+
+  let isSpade card = isOf card Color.Spade
+
+  let isHeart card = isOf card Color.Heart
+
+  let isDiamond card = isOf card Color.Diamond
+
+  let isClub card = isOf card Color.Club
+end
+
+let () =
+  let open Card in
+
+  let card1 = newCard Value.T7 Color.Heart in
+  let card2 = newCard Value.T10 Color.Spade in
+  let card3 = newCard Value.Jack Color.Diamond in
+
+  (* toString and toStringVerbose *)
+  Printf.printf "Card 1: %s\n" (toString card1); (* 7H *)
+  Printf.printf "Card 2: %s\n" (toString card2); (* 10S *)
+  Printf.printf "Card 3: %s\n" (toStringVerbose card3); (* Card(Jack, Diamond) *)
+
+  (* Compare *)
+  Printf.printf "Comparison between card1 and card2: %d\n" (compare card1 card2); (* negative *)
+  Printf.printf "Comparison between card2 and card3: %d\n" (compare card2 card3); (* positive *)
+
+  (* Max and Min *)
+  let max_card = max card1 card2 in
+  let min_card = min card2 card3 in
+  Printf.printf "Max card: %s\n" (toString max_card); (* 10S *)
+  Printf.printf "Min card: %s\n" (toString min_card); (* 10S *)
+
+  (* Best *)
+  let cards = [card1; card2; card3] in
+  let best_card = best cards in
+  Printf.printf "Best card: %s\n" (toString best_card); (* 10S *)
+
+  (* Color checks *)
+  Printf.printf "Is card1 a Heart? %b\n" (isHeart card1); (* true *)
+  Printf.printf "Is card2 a Spade? %b\n" (isSpade card2); (* true *)
+  Printf.printf "Is card3 a Diamond? %b\n" (isDiamond card3); (* true *)
+  Printf.printf "Is card1 a Club? %b\n" (isClub card1);; (* false *)
