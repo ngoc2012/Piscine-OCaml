11aa98e719ef7d91412195b47e94b8fe1241caf5
minh-ngu <minh-ngu@ngoc.myguest.virtualbox.org>
2025-01-30 12:50:02 +0100

    tempory

--- 11/ex03/jokes.ml
+++ 11/ex03/jokes.ml
@@ -1,15 +1,3 @@
-let read_jokes_from_file filename =
-  let ic = open_in filename in
-  let rec read_lines acc =
-    try
-      let line = input_line ic in
-      read_lines (line :: acc)
-    with End_of_file -> 
-      close_in ic;
-      List.rev acc
-  in
-  read_lines []
-
 let () =
   (* Ensure that the program receives exactly one command-line argument *)
   if Array.length Sys.argv <> 2 then (
@@ -18,12 +6,24 @@ let () =
   );
   let filename = Sys.argv.(1) in
   try
-    let jokes = read_jokes_from_file filename in
+    let read_file filename =
+      let ic = open_in filename in
+      let rec read_lines acc =
+        try
+          let line = input_line ic in
+          read_lines (line :: acc)
+        with End_of_file -> 
+          close_in ic;
+          List.rev acc
+      in
+      read_lines []
+    in
+    let jokes = read_file filename in
     if List.length jokes = 0 then
       Printf.eprintf "No jokes found in the file.\n"
     else
-      let random_joke = List.nth jokes (Random.int (List.length jokes)) in
-      print_endline random_joke
+      Random.self_init ();
+      print_endline (List.nth jokes (Random.int (List.length jokes)))
   with
   | Sys_error err -> Printf.eprintf "Error: %s\n" err
   | _ -> Printf.eprintf "An unexpected error occurred.\n"
