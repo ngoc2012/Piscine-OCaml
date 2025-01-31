0501a1b36890e65aa01bf74b85b5ab220b0d8eb0
minh-ngu <minh-ngu@ngoc.myguest.virtualbox.org>
2025-01-31 11:30:02 +0100

    tempory

--- 11/ex06/examples_of_file.ml
+++ 11/ex06/examples_of_file.ml
@@ -14,19 +14,19 @@ let examples_of_file filename =
     	  read_lines ((parse_input line) :: acc)
         else
           read_lines acc
-    with End_of_file ->
-      close_in file;
-      List.rev acc
+    with
+      | End_of_file ->
+        close_in file;
+        List.rev acc
+      | _ -> Printf.eprintf "An unexpected error occurred.\n"
   in
   read_lines []
 
 let () =
-  if (Array.length Sys.argv) < 2 then
-    Printf.printf "Usage: %s <filename>\n" Sys.argv.(0)
-  else
-    let data = examples_of_file Sys.argv.(1) in
-    List.iter (fun (arr, lbl) ->
-      Printf.printf "Array: [|";
-      Array.iter (fun x -> Printf.printf "%.1f; " x) arr;
-      Printf.printf "|], Label: %s\n" lbl
-    ) data
+  let data = examples_of_file "ionosphere.train.csv" in
+  List.iter (fun (arr, lbl) ->
+    Printf.printf "Array: [|";
+    Array.iter (fun x -> Printf.printf "%.5f; " x) arr;
+    Printf.printf "|], Label: %s\n" lbl
+  ) data;
+  print_endline ("Number of examples: " ^ (string_of_int (List.length data)));;
